e43c3720914a7b68bc267cb5bb87307c9f8f585f
chao <mailbox.chao.cao@gmail.com>
2018-04-30 15:48:09 -0700

    Complete array project

--- dynamic_arrays/lib/ring_buffer.rb
+++ dynamic_arrays/lib/ring_buffer.rb
@@ -4,30 +4,56 @@ class RingBuffer
   attr_reader :length
 
   def initialize
+    @length = 0
+    @capacity = 8
+    @start = 0
+    @store = StaticArray.new(@capacity)
   end
 
   # O(1)
   def [](index)
+    check_index(index)
+    target = (@start + index) % @capacity
+    @store[target]
   end
 
   # O(1)
   def []=(index, val)
+    check_index(index)
+    target = (@start + index) % @capacity
+    @store[target] = val
   end
 
   # O(1)
   def pop
+    raise "index out of bounds" if @length == 0
+    popped = @store[(@start + @length) % capacity - 1]
+    @length -= 1
+    popped
   end
 
   # O(1) ammortized
   def push(val)
+    resize! if @length == capacity
+    @store[(@start + length) % capacity] = val
+    @length += 1
   end
 
   # O(1)
   def shift
+    raise "index out of bounds" if @length == 0
+    shifted = @store[@start]
+    @start = (@start + 1) % @capacity
+    @length -= 1
+    shifted
   end
 
   # O(1) ammortized
   def unshift(val)
+    resize! if @length == capacity
+    @start = (@start - 1) % @capacity
+    @length += 1
+    @store[@start] = val
   end
 
   protected
@@ -35,8 +61,19 @@ class RingBuffer
   attr_writer :length
 
   def check_index(index)
+    if index >= @length
+      raise "index out of bounds"
+    end
   end
 
   def resize!
+    new_capacity = @capacity * 2
+    temp_store = StaticArray.new(new_capacity)
+    @length.times do |idx|
+      temp_store[idx] = @store[(@start + idx) % capacity]
+    end
+    @start = 0
+    @capacity = new_capacity
+    @store = temp_store
   end
 end
